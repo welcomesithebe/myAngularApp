fbd651b82081d57382d613ff68c2a404df1dd963
Welcome <welcomesithebe@thedigitalacademy.co.za>
2019-06-27 16:13:27 +0200

    users works

--- myAngularApp2/src/app/app.module.ts
+++ myAngularApp2/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { PostsComponent } from './posts/posts.component';
+import { UsersComponent } from './users/users.component';
+import { DetailComponent } from './detail/detail.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    PostsComponent,
+    UsersComponent,
+    DetailComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
